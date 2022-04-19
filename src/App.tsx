01f634317a65d2a81dc7264e2cef4a29d5f88c7a
subramanya-ec18 <subramanya.ec18@bmsce.ac.in>
2022-04-19 21:55:31 +0530

    Added basic structure📏

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import "./App.css";
+import Body from "./Components/Body/Body";
+import Footer from "./Components/Footer/Footer";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default function NavBar() {
+  return <div>NavBar</div>;
+}
